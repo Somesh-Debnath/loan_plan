0580e048f6e94b612f9ca9c6fb02762a12f9b6e9
Somesh-Debnath <debnathsomesh1@gmail.com>
2023-05-17 23:12:03 +0530

    code

---
+++ src/main/java/com/cts/loan_plan/LoanPlanApplication.java
@@ -0,0 +1,13 @@
+package com.cts.loan_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cts/loan_plan/LoanPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.loan_plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
